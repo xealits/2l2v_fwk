eaac686f13858db458e78d70e44709ff9a56ac1f
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2015-12-10 18:20:43 +0000

    Merged checking the proxy validity into 1 if
    Note, there is a question (in TODO) about the command and checking minutes instead of hours.

--- scripts/runAnalysisOverSamples_2w.py
+++ scripts/runAnalysisOverSamples_2w.py
@@ -121,10 +121,9 @@ if "iihe.ac.be" in hostname: localTier = "T2_BE_IIHE"
 if "cern.ch" in hostname:    localTier = "T2_CH_CERN"
 
 
-initialCommand = ''
-validCertificate = True
-# --voms cms, otherwise it does not work normally
+# checking if the proxy needs renewal
 voms_proxy_filename = expanduser(PROXYDIR + '/x509_proxy')
+# --voms cms, otherwise it does not work normally
 voms_proxy_timeleft_cmd = '(export X509_USER_PROXY='+PROXYDIR+'/x509_proxy; voms-proxy-init --voms cms --noregen; voms-proxy-info -all) | grep timeleft | tail -n 1'
 voms_proxy_timeleft  = int(commands.getstatusoutput(voms_proxy_timeleft_cmd)[1].split(':')[2])
 # TODO: checked the voms_proxy_timeleft_cmd output:
@@ -134,10 +133,7 @@ voms_proxy_timeleft  = int(commands.getstatusoutput(voms_proxy_timeleft_cmd)[1].
 # >>> getstatusoutput('voms-proxy-info -all | grep timeleft | tail -n 1')[1].split(':')
 # ['timeleft  ', ' 06', '49', '56']
 if not isfile(voms_proxy_filename) or time() - getmtime(voms_proxy_filename) > 600 or voms_proxy_timeleft < 8:
-    validCertificate = False
-# TODO: now actually these two ifs can be merged
-if not validCertificate:
-    # then let's renew it
+    # then let's renew the proxy
     print "You are going to run on a sample over grid using either CRAB or the AAA protocol, it is therefore needed to initialize your grid certificate"
     voms = 'cms:/cms/becms' if "iihe.ac.be" in hostname else 'cms'
     # calling the proxy init command:
